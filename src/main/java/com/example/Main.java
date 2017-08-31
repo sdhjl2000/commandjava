72f394d2b075e17270a4d17aa3a797e572b4e621
胡金良 <hujinliang@autohome.com.cn>
2017-08-31 11:12:36 +0800

    333

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 import com.example.lambda.Function;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
@@ -12,11 +13,16 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 public class Main {
     public static void main(String[] args) throws  Exception{
         //check whether Java8 syntax compiles
-        CuratorFramework client = CuratorFrameworkFactory.newClient("108.61.247.160:2181", new ExponentialBackoffRetry(1000, 3));
+        CuratorFramework client = CuratorFrameworkFactory.newClient("108.61.247.160:2181", 1000,5000, new ExponentialBackoffRetry(1000, 3));
         client.start();
         InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(client, "/SyncEsData");
-        lock.acquire();
-        lock.release();
+        InterProcessMutex interProcessMutex=new InterProcessMutex(client, "/SyncEsData");
+        System.out.print("-----try get");
+        interProcessMutex.acquire();
+        System.out.print("----- get");
+        Thread.sleep(10000);
+        System.out.print("----- after sleep");
+        System.out.print(interProcessMutex.isAcquiredInThisProcess());
     }
 
     static Function printHello = () -> {
